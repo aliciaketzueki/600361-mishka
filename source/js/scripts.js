70f461db7dee4a12bcf3c47cdf99b9a5ea78ef66
Aliciaketzueki <aliciaketzueki@gmail.com>
2018-05-05 01:03:42 +0300

    Добавлен скрипт открытия модального окна

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,11 +1,25 @@
 var button1 = document.querySelector(".nav__button");
 var popup1 = document.querySelector(".nav__list--nav");
 var popup2 = document.querySelector(".nav__list--tools");
-var button2 = document.querySelector(".nav__button--closed")
+var button2 = document.querySelector(".nav__button--closed");
+
+var cart = document.querySelector(".cart");
+var modal = document.querySelector(".modal");
+var add = document.querySelector(".modal__button");
 
 button1.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup1.classList.toggle("nav__list--show");
     popup2.classList.toggle("nav__list--show");
     button2.classList.toggle("nav__button--opened");
+});
+
+cart.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.add("modal-show");
+});
+
+add.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal-show");
 });
